1612e0684a62cc102a052410c9c2091cdd0b4661
Ryan Whitmire <ryanw51@gmail.com>
2013-06-07 19:53:24 -0400

    first commit

---
+++ TodoWpf/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using Elysium;
+
+namespace MetroApplication1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void StartupHandler(object sender, StartupEventArgs e)
+        {
+            this.Apply(Theme.Light, AccentBrushes.Blue, Brushes.White);
+        }
+    }
+}
