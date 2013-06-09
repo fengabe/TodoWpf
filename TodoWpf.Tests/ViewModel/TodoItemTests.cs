1f7e1c71329cc84b6ebec0583fd1eb662c0532d3
Ryan Whitmire <ryanw51@gmail.com>
2013-06-09 16:18:29 -0400

    added todoitem editing

--- TodoWpf.Tests/ViewModel/TodoItemTests.cs
+++ TodoWpf.Tests/ViewModel/TodoItemTests.cs
@@ -1,4 +1,5 @@
-﻿using TodoWpf.ViewModel;
+﻿using System.Windows;
+using TodoWpf.ViewModel;
 using Xunit;
 
 namespace TodoWpf.Tests.ViewModel
@@ -25,6 +26,90 @@ namespace TodoWpf.Tests.ViewModel
             };
 
             Assert.Equal("#000000", todoItem.ContentColor);
+        }
+
+        [Fact]
+        public void ClickingLabelShouldPutViewModelInEditMode()
+        {
+            var todoItem = new TodoItemViewModel
+            {
+                EditMode = false
+            };
+
+            todoItem.LabelClickedCommand.Execute(new {});
+
+            Assert.True(todoItem.EditMode);
+        }
+
+        [Fact]
+        public void TextboxShouldBeVisibleWhenInEditMode()
+        {
+            var todoItem = new TodoItemViewModel
+            {
+                EditMode = true
+            };
+
+            Assert.Equal(Visibility.Visible, todoItem.TextboxVisibility);
+        }
+
+        [Fact]
+        public void LabelShouldBeHiddenWhenInEditMode()
+        {
+            var todoItem = new TodoItemViewModel
+            {
+                EditMode = true
+            };
+
+            Assert.Equal(Visibility.Hidden, todoItem.LabelVisibility);
+        }
+
+        [Fact]
+        public void TexboxShouldBeHiddenWhenNotInEditMode()
+        {
+            var todoItem = new TodoItemViewModel
+            {
+                EditMode = false
+            };
+
+            Assert.Equal(Visibility.Hidden, todoItem.TextboxVisibility);
+        }
+
+        [Fact]
+        public void LabelShouldBeVisibleWhenNotInEditMode()
+        {
+            var todoItem = new TodoItemViewModel
+            {
+                EditMode = false
+            };
+
+            Assert.Equal(Visibility.Visible, todoItem.LabelVisibility);
+        }
+
+        [Fact]
+        public void LabelShouldBeShownByDefault()
+        {
+            var todoItem = new TodoItemViewModel();
+            Assert.Equal(Visibility.Visible, todoItem.LabelVisibility);
+        }
+
+        [Fact]
+        public void TextboxShouldBeHiddenByDefault()
+        {
+            var todoItem = new TodoItemViewModel();
+            Assert.Equal(Visibility.Hidden, todoItem.TextboxVisibility);
+        }
+
+        [Fact]
+        public void EnterKeyShouldEndEditMode()
+        {
+            var todoItem = new TodoItemViewModel
+            {
+                EditMode = true
+            };
+
+            todoItem.EnterKeyCommand.Execute(new {});
+
+            Assert.False(todoItem.EditMode);
         }
     }
 }
--- TodoWpf/ViewModel/TodoItemViewModel.cs
+++ TodoWpf/ViewModel/TodoItemViewModel.cs
@@ -1,15 +1,28 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Windows;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace TodoWpf.ViewModel
 {
     public class TodoItemViewModel : ViewModelBase
     {
+        public TodoItemViewModel()
+        {
+            LabelClickedCommand = new RelayCommand(() => EditMode = true);
+            EnterKeyCommand = new RelayCommand(() => EditMode = false);
+            EditMode = false;
+        }
+
+        private const string INCOMPLETE_COLOR = "#000000";
+        private const string COMPLETE_COLOR = "#BBBBBB";
+
         private string _description;
         private bool _complete;
         private string _contentColor;
-        private const string INCOMPLETE_COLOR = "#000000";
-        private const string COMPLETE_COLOR = "#BBBBBB";
-        
+        private bool _editMode;
+        private Visibility _labelVisibility;
+        private Visibility _textboxVisibility;
+
         public string Description
         {
             get { return _description; }
@@ -38,6 +51,51 @@ namespace TodoWpf.ViewModel
             {
                 _contentColor = value;
                 RaisePropertyChanged(() => ContentColor);
+            }
+        }
+
+        public RelayCommand LabelClickedCommand { get; set; }
+
+        public RelayCommand EnterKeyCommand { get; set; }
+
+        public bool EditMode
+        {
+            get { return _editMode; }
+            set
+            {
+                _editMode = value;
+                RaisePropertyChanged(() => EditMode);
+
+                if (_editMode)
+                {
+                    TextboxVisibility = Visibility.Visible;
+                    LabelVisibility = Visibility.Hidden;
+                }
+                else
+                {
+                    TextboxVisibility = Visibility.Hidden;
+                    LabelVisibility = Visibility.Visible;
+                }
+            }
+        }
+
+        public Visibility LabelVisibility
+        {
+            get { return _labelVisibility; }
+            private set
+            {
+                _labelVisibility = value;
+                RaisePropertyChanged(() => LabelVisibility);
+            }
+        }
+
+        public Visibility TextboxVisibility
+        {
+            get { return _textboxVisibility; }
+            private set
+            {
+                _textboxVisibility = value;
+                RaisePropertyChanged(() => TextboxVisibility);
             }
         }
     }
